a1e102c7793df6c4c6c439e74df5238001db0be7
marcophm <tanh6772@gmail.com>
2024-10-22 02:15:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = [" "];
+const statusMessages = ["Created By X Team 🇻🇳"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
